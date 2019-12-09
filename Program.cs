9ea5f3be564280a314c4a5bb93ae2505c8acc0ee
cheng li <ccie20079@126.com>
2019-12-09 13:41:10 +0800

    2019-12-09 13:41

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace AttendanceRecord
+{
+    public static class Program
+    {
+        #region Version Info
+        //=====================================================================
+        // Project Name        :    BaseDao  
+        // Project Description : 
+        // Class Name          :    Class1
+        // File Name           :    Class1
+        // Namespace           :    BaseDao 
+        // Class Version       :    v1.0.0.0
+        // Class Description   : 
+        // CLR                 :    4.0.30319.42000  
+        // Author              :    董   魁  (ccie20079@126.com)
+        // Addr                :    中国  陕西 咸阳    
+        // Create Time         :    2019-10-22 14:57:19
+        // Modifier:     
+        // Update Time         :    2019-10-22 14:57:19
+        //======================================================================
+        // Copyright © DGCZ  2019 . All rights reserved.
+        // =====================================================================
+        #endregion
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //nothing to do 
+            ;
+        }
+    }
+}
